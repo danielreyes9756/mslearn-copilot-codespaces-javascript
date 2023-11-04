ef1705ee31f7244be5cca4734bbe025eabd2ae78
Daniel Reyes García <43205522+danielreyes9756@users.noreply.github.com>
2023-11-04 17:23:00 +0000

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Daniel Reyes García",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "daniel.reyes107@alu.ulpgc.com",
+  gitHub: "danielreyes9756",
+  instagram: "@danielreyes9756",
+  linkedIn: "danielreyes9756",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
